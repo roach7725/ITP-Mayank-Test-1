4a578c08614e677d0770db8d3282610e930c31c8
roach7725 <chapanerimayank5@gmail.com>
2023-03-31 09:14:14 -0400

    v2

--- problem1.py
+++ problem1.py
@@ -3,3 +3,4 @@ Patientname=input(("Enter patient's name: "))
 Cleaning=input("Was cleaning performed? (y/n): ")
 Cavity=input("Was cavity-filling performed? (y/n): ") 
 x_ray=input("Was X-Ray performed? (y/n): ")
+a=1
