2adb160f81838fde0e9b5a9d51eff6dc81c69fdb
roach7725 <chapanerimayank5@gmail.com>
2023-03-31 09:12:57 -0400

    v1

---
+++ problem1.py
@@ -0,0 +1,5 @@
+print(("Melanie Dental Clinic").center(100))
+Patientname=input(("Enter patient's name: "))
+Cleaning=input("Was cleaning performed? (y/n): ")
+Cavity=input("Was cavity-filling performed? (y/n): ") 
+x_ray=input("Was X-Ray performed? (y/n): ")
