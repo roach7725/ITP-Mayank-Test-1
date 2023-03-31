d00ee1b78d3d801cbecc79f27231894e3132b3aa
roach7725 <chapanerimayank5@gmail.com>
2023-03-31 11:11:11 -0400

    v2

--- problem3.py
+++ problem3.py
@@ -13,13 +13,15 @@ total_cents = (pennies * PENNY_VALUE) + (nickels * NICKEL_VALUE) + \
               (dimes * DIME_VALUE) + (quarters * QUARTER_VALUE)
 
 
-total_dollars = total_cents / PENNIES_IN_DOLLAR
+total_dollars = total_cents 
 print("Enter the number of pennies :",pennies)
 print("Enter the number of nickels :",nickels)
 print("Enter the number of dimes :",dimes)
 print("Enter the number of quaters :",quarters)
-if total_dollars > 1.0:
+if total_dollars > 100:
     print("Sorry, the amount you entered was more than one dollar.")
+if total_dollars<100:
+    print("Sorry, the amount you entered was less than one dollar.")
 
 else:
     print("Congratulations!")
